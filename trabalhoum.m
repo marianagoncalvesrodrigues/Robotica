90ccce4b2a747bde484fa32170894822dd226d94
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2023-11-07 14:27:59 -0300

    Update trabalhoum.m

--- trabalhoum.m
+++ trabalhoum.m
@@ -1,38 +1,24 @@
-%% Parte 1
+
+%% Exercício 1
 %-----------------------------------------
 % Join  |  theta | d  | a  | alfa | sigma |
+%  0    |    q0  | 0  | 0  |   0  |  0    |
 %  1    |    q1  | 50 | 0  |   0  |  0    |
-%  2    |    q2  | 0  | 0  |  -90 |  0    |
+%  2    |    q2  | 0  | 0  | -90  |  0    |
 %  3    |    q3  | 0  | 50 |   0  |  0    |
-%  4    |    q4  | 0  | 50 |   0  |  0    |
-%  5    |    q5  | 5  | 0  |  -90 |  0    |
+%  4    |    q4  | 0  | 50 | -90  |  0    |
+%  5    |    q5  | 5  | 0  |   0  |  0    |
 %-----------------------------------------
-clc
-clear all
-t1 = "q1";
-d1 = 50; 
-a1 = 0; 
-alpha1 = 0; 
+a0 = 0; alpha0 = 0; d0 = 0; theta0 = "q0";
+a1 = 0; alpha1 = 0; d1 = 50; theta1 = "q1";
+a2 = 50; alpha2 = -pi/2; d2 = 0; theta2 = "q2";
+a3 = 50; alpha3 = 0; d3 = 0; theta3 = "q3";
+a4 = 0; alpha4 = -pi/2; d4 = 0; theta4 = "q4";
+a5 = 0; alpha5 = 0; d5 = 5; theta5 = "q5";
 
-t2 = "q2";
-d2 = 0;
-a2 = 0; 
-alpha2 = -pi/2;
-
-t3 = "q3";
-d3 = 0;
-a3 = 50; 
-alpha3 = 0;
-
-t4 = "q4";
-d4 = 0;
-a4 = 50; 
-alpha4 = 0;
-
-t5 = "q5";
-d5 = 5;
-a5 = 0; 
-alpha5 = -pi/2;  
+link0 = rigidBody("link0");
+link0.Joint = rigidBodyJoint("joint0","revolute");
+link0.Joint.setFixedTransform([a0 alpha0 d0 0],"dh");
 
 link1 = rigidBody("link1");
 link1.Joint = rigidBodyJoint("joint1","revolute");
@@ -55,8 +41,8 @@ link5.Joint = rigidBodyJoint("joint5","revolute");
 link5.Joint.setFixedTransform([a5 alpha5 d5 0],"dh");
 
 myRobot = rigidBodyTree(DataFormat="row");
-myRobot.addBody(link1,myRobot.BaseName);
-
+myRobot.addBody(link0,myRobot.BaseName);
+myRobot.addBody(link1, link0.Name);
 myRobot.addBody(link2,link1.Name);
 myRobot.addBody(link3,link2.Name);
 myRobot.addBody(link4,link3.Name);
@@ -64,120 +50,64 @@ myRobot.addBody(link5,link4.Name);
 
 myRobot.showdetails
 myRobot.show
-
-%% Parte 2
-
-a     = [a1,         a2,     a3,     a4,     a5];
-alpha = [alpha1, alpha2, alpha3, alpha4, alpha5];
-d     = [d1,         d2,     d3,     d4,     d5];
-theta = [0,           0,      0,      0,      0]; 
-
-%Inicializa a matriz de transformação MatrizTransf com uma matriz de identidade 4x4. Esta matriz acumulará as transformações ao longo das juintas
-MatrizTransf = eye(4); 
-%Armazena as matrizes de transformação para cada junta. Isso é útil para visualizar as transformações intermediárias.
-MatrizesTransf = cell(1, numel(a));
-
-for i = 1:numel(a)
-    T = [
-        cos(theta(i)), -sin(theta(i)) * cos(alpha(i)), sin(theta(i)) * sin(alpha(i)), a(i) * cos(theta(i));
-        sin(theta(i)), cos(theta(i)) * cos(alpha(i)), -cos(theta(i)) * sin(alpha(i)), a(i) * sin(theta(i));
-        0, sin(alpha(i)), cos(alpha(i)), d(i);
-        0, 0, 0, 1;
+%% Definindo os parametros DH (Denavit-Hartenberg) para cada junta do robo
+% Cada linha da matriz dh_parameters corresponde a um elo (junta) do robo
+% A matriz dh_parameters tem quatro colunas: a, alpha, d e theta
+% a: Comprimento do elo
+% alpha: angulo entre x_i-1 e x_i ao redor de z_i-1 (angulo de torcao)
+% d: distancia entre x_i-1 e x_i ao longo de z_i-1
+% theta: angulo de rotacao da junta (variavel de junta).
+T = eye(4); % matriz de transformacao homogenea T como matriz identidade 4x4
+for i = 1:size(dh_parameters, 1) % matrizes de transformacao homogenea para cada junta e multiplicacao para obter a transformacao total do efetuador
+    a = dh_parameters(i, 1);
+    alpha = dh_parameters(i, 2);
+    d = dh_parameters(i, 3);
+    theta = dh_parameters(i, 4);
+    
+    % Matriz de transformacao homogenea A para a i-esima junta
+    A = [
+        cos(theta), -sin(theta) * cos(alpha), sin(theta) * sin(alpha), a * cos(theta);
+        sin(theta), cos(theta) * cos(alpha), -cos(theta) * sin(alpha), a * sin(theta);
+        0, sin(alpha), cos(alpha), d;
+        0, 0, 0, 1
     ];
-    MatrizTransf = MatrizTransf * T;
-    MatrizesTransf{i} = MatrizTransf;
+    
+    % Multiplicacao da matriz de transformacao A com a matriz T
+    T = T * A;
+    
+    % Exibicao da matriz de transformacao homogenea da junta atual
+    fprintf('Matriz de transformação homogênea da Junta %d:\n', i);
+    disp(A);
+    fprintf('\n');
 end
-
-
-for i = 1:numel(MatrizesTransf)
-    fprintf('Matriz de transformação para a junta %d:\n', i);
-    disp(MatrizesTransf{i});
+%% Exercicio 3
+% Exibicao da matriz de transformacao homogenea total do efetuador em funcao dos angulos das juntas
+fprintf('Matriz de transformacao homogenea do atuador em função dos angulos das juntas:\n');
+disp(T);
+%% Exercicio 4 e 5
+% Criacao de um ETS3 que descreve a cinematica direta do robo com base nos angulos das juntas
+e = ETS3.Rz("q1")*ETS3.Tz(50)*ETS3.Ry("q2")*ETS3.Tx(50)*ETS3.Ry("q3")*ETS3.Tx(50)*ETS3.Ry("q4")*ETS3.Tx(5)*ETS3.Ry("q5");
+% Definicao de pontos de referencia A, B e C para um trajeto
+A=[50 0 70];
+B=[50 30 20];
+C=[50 -30 20];
+waypts = [A' B' C' A']; % Concatenacao dos waypoints para criar uma matriz waypts
+t = 0:0.05:10; % t de 0 a 10 segundos com incremento de 0.05 s
+[q2, qd2, qdd2] = trapveltraj(waypts, numel(t), EndTime=10); %trajetoria de velocidade trapezoidal (q2, qd2 e qdd2) para os waypoints
+r=rateControl(1000000); %controlador de taxa para controlar a exibicao do robo
+% Inicializacao de variaveis de iteracao e configuracao inicial das juntas q
+i = 1;
+q = [0 0 0 0 0];
+%ajuste das juntas do robo e exibir a trajetoria planejada
+while(1)
+     % Otimizacao dos angulos das juntas para se aproximarem dos angulos desejados (q2)
+     q = fminsearch(@(q) norm([se3(e.fkine(q)).trvec se3(e.fkine(q)).eul]-[q2(1:3,i)' 0 0 0]), q); 
+     % Exibicao do robo na configuracao atual
+     e.plot(q,'jointdiam',1)
+     r.waitfor;
+     % Verificacao de termino da trajetoria
+     if i == size(q2, 2)
+         i=0;
+     end
+     i = i + 1;
 end
-
-%% Parte 3
-
-MatrizesTransf = cell(1, numel(a) + 1);
-MatrizesTransf{numel(a) + 1} = MatrizTransf;
-for i = 1:numel(a)
-    T = [
-        cos(theta(i)), -sin(theta(i)) * cos(alpha(i)), sin(theta(i)) * sin(alpha(i)), a(i) * cos(theta(i));
-        sin(theta(i)), cos(theta(i)) * cos(alpha(i)), -cos(theta(i)) * sin(alpha(i)), a(i) * sin(theta(i));
-        0, sin(alpha(i)), cos(alpha(i)), d(i);
-        0, 0, 0, 1;
-    ];
-    MatrizTransf = MatrizTransf * T;
-    MatrizesTransf{i} = MatrizTransf;
-end
-disp('Matriz de transformação para o atuador:');
-disp(MatrizesTransf{end});
-
-%% Parte 4
-clear all
-close all
-
-syms a1 a2 real
-e = ETS2.Tx(50) * ETS2.Rz("q1") * ETS2.Tx(50) * ETS2.Tx(50) * ETS2.Rz("q2") * ETS2.Tx(5);
-
-syms q1 q2 q3 real
-TE = e.fkine([q1 q2 q3])
-transl = TE(1:2,3)';
-
-syms x y real
-e1 = x == transl(1)
-e2 = y == transl(2)
-
-[s1,s2,s3] = solve([e1 e2],[q1 q2 q3])
-
-length(s2)
-
-%% Parte 5
-% Definição das variáveis simbólicas
-syms a1 a2 real
-e = ETS2.Tx(50) * ETS2.Rz("q1") * ETS2.Tx(50) * ETS2.Tx(50) * ETS2.Rz("q2") * ETS2.Tx(5);
-
-% Define as equações para a cinemática inversa
-syms q1 q2 q3 real
-TE = e.fkine([q1 q2 q3]);
-transl = TE(1:3, 3);
-
-% Define as coordenadas dos vértices do triângulo
-A = [50; 0; 70];
-B = [50; 30; 20];
-C = [50; -30; 20];
-
-% Define as equações para a cinemática inversa
-e1 = A == transl(1);  % Substitua para B e C em iterações posteriores
-e2 = B == transl(2);
-e3 = C == transl(3);
-
-% Resolve o sistema de equações numericamente
-[s1, s2, s3] = vpasolve(e1, [q1, q2, q3]);
-
-%
-q=[0 0 0 0 0 0]
-r= rateControl(10);
-qq = [linspace(0,6*pi,1000)'];
-for i = 1:size(qq,1)
-   pstar = [1 .70+0.20*cos(qq(i)) .70+0.20*sin(qq(i))]; % Desired position
-q = fminsearch(@(q) norm(se3(e.fkine(q)).trvec-pstar),q)
-rad2deg(q)
-e.plot(q, 'workspace', [-1 1 -1 1 -1 1])
-   r.waitfor;
-end
-%%
-e =ETS2.Tx(50) * ETS2.Rz("q1") * ETS2.Tx(50) * ETS2.Tx(50) * ETS2.Rz("q2") * ETS2.Tx(5) * ETS2.Rz("q3");
-
-q = [0 0 0];
-r = rateControl(10);
-g = linspace(0, pi, 100)';
-for i = 1:size(g,1)
-pstar = [50 0 -30]; %posição desejada
-q = fminsearch(@(q) norm([se3(e.fkine(q)).trvec se3(e.fkine(q)).eul]-pstar),q);
-rad2deg(q)
-%axis([0 100 0 100]
-e.plot(q, 'workspace', [-1000 1000 -1000], "scale", 0.1)
-
-r.waitfor;
-end
-
-
